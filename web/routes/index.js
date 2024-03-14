beb819f215087e3186318805366b2bd526b3d85b
kenu <kenu@okjsp.pe.kr>
2024-03-15 02:11:40 +0900

    en refactor

--- web/routes/index.js
+++ web/routes/index.js
@@ -7,6 +7,19 @@ const router = express.Router()
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const list = await dao.findAllYoutube()
+  building(list)
+  res.render('index', { title: '개발 관련 유튜브', list })
+})
+
+/* GET home page. */
+router.get('/en', async function (req, res, next) {
+  const list = await dao.findAllYoutube('en')
+  building(list)
+  res.render('index', { title: 'YouTube for Developers', list })
+})
+
+export default router
+function building(list) {
   list.forEach((item) => {
     item.pubdate = dayjs(item.publishedAt).format('YYYY-MM-DD')
     item.profile = item.Channel.dataValues.thumbnail
@@ -14,16 +27,5 @@ router.get('/', async function (req, res, next) {
     item.customUrl = item.Channel.dataValues.customUrl
     delete item.Channel
   })
-  res.render('index', { title: '개발 관련 유튜브', list })
-})
+}
 
-/* GET home page. */
-router.get('/en', async function (req, res, next) {
-  const list = await dao.findAllYoutube('en')
-  list.forEach((item) => {
-    item.pubdate = dayjs(item.publishedAt).format('YYYY-MM-DD')
-  })
-  res.render('indexen', { title: 'YouTube for Developers', list })
-})
-
-export default router
