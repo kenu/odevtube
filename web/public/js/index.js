f9462f29293dddabd60c5166b543a3857f3562df
kenu <kenu@okjsp.pe.kr>
2024-03-15 04:23:54 +0900

    font link

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -21,6 +21,33 @@ function search(evt) {
 window.onload = function () {
   document.getElementById('count').innerHTML =
     document.querySelectorAll('li').length
+
   const el = document.getElementById('keyword')
   el.addEventListener('keyup', search)
+
+  document.querySelector('h1+p').addEventListener('click', function () {
+    changeFont()
+  })
+
+  function changeFont() {
+    const font = new FontFace(
+      'KyoboHand',
+      'url(https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/KyoboHand.woff)',
+      {}
+    )
+
+    font
+      .load()
+      .then(function (loadedFont) {
+        document.fonts.add(loadedFont)
+        const setFont = localStorage.getItem('font')
+        localStorage.setItem('font', setFont ? '' : 'true')
+        document.body.style.fontFamily = setFont
+          ? 'KyoboHand, sans-serif'
+          : 'Roboto, Arial, sans-serif'
+      })
+      .catch(function (error) {
+        console.error('Failed to load font:', error)
+      })
+  }
 }
