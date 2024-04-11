0732394f965f0a774052c049585b6744e510e96b
kenu <kenu@okjsp.pe.kr>
2024-04-12 05:17:50 +0900

    rm failed tests

--- tests/transcript.test.js
+++
@@ -1,7 +0,0 @@
-import fetchTranscript from '../web/transcript.js'
-
-test('', async() => {
-  const videoId = '4Z8qAzlnue8'
-  const transcript = await fetchTranscript(videoId)
-  console.log(transcript)
-})
