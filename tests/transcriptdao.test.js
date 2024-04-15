4e803856e2532176141c23abf5df096ffdde1b78
kenu <kenu@okjsp.pe.kr>
2024-04-16 07:29:27 +0900

    🧪 test dao

--- tests/transcriptdao.test.js
+++ tests/transcriptdao.test.js
@@ -19,8 +19,28 @@ test('save with videoId', async () => {
   }
   const result = await dao.createTranscript(data)
   expect(result).not.toBeNull()
+  const resultnull = await dao.createTranscript({})
+  expect(resultnull).toBeUndefined()
 })
 
 afterEach(async () => {
   return await dao.removeTranscript(videoId);
 });
+
+test('newList', async () => {
+  const newList = await dao.newList()
+  expect(newList).not.toBeNull()
+  expect(newList.length).toBe(0)
+})
+
+test('findOneByChannelId', async () => {
+  const channelId = 'UC_x5XG1OV2P6uZZ5FSM9Ttw'
+  const channel = await dao.findOneByChannelId(channelId)
+  expect(channel).toBeNull()
+})
+
+test('findAndCountAllYoutube', async () => {
+  const result = await dao.findAndCountAllYoutube()
+  expect(result).not.toBeNull()
+  expect(result.count).not.toBe(0)
+})
