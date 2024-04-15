106115c1dd50f0d22ee2da60252e9aa62d2369c3
kenu <kenu@okjsp.pe.kr>
2024-04-15 21:51:16 +0900

    🐛 fix bodyParser

--- web/bin/youtb.js
+++ web/bin/youtb.js
@@ -3,7 +3,7 @@
 import app from '../app.js'
 import http from 'http'
 
-const port = normalizePort(process.env.PORT || '3000')
+const port = normalizePort(process.env.PORT || '4000')
 app.set('port', port)
 
 /**
