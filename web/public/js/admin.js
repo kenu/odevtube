1dece4b12f54307a361e9a91cbe09d1f4349b21f
kenu <kenu@okjsp.pe.kr>
2024-04-26 04:17:08 +0900

    ✨ table sorting

--- web/public/js/admin.js
+++ web/public/js/admin.js
@@ -11,13 +11,56 @@ function createChannel() {
     },
     body: JSON.stringify(data),
   })
-   .then((res) => res.json())
-   .then((res) => {
+    .then((res) => res.json())
+    .then((res) => {
       console.log(res)
       alert('success: ' + JSON.stringify(res))
       document.getElementById('channelId').value = ''
     })
-   .catch((err) => {
+    .catch((err) => {
       console.error(err)
     })
+}
+
+function sortTable(n) {
+  const table = document.getElementById('channelTable')
+  let switchcount = 0
+  let switching = true
+  let shouldSwitch = true
+  let dir = 'asc'
+  let i = 0
+
+  while (switching) {
+    switching = false
+    const rows = table.rows
+
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false
+      const x = rows[i].getElementsByTagName('TD')[n]
+      const y = rows[i + 1].getElementsByTagName('TD')[n]
+
+      if (dir == 'asc') {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      } else if (dir == 'desc') {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+      switching = true
+      switchcount++
+    } else {
+      if (switchcount == 0 && dir == 'asc') {
+        dir = 'desc'
+        switching = true
+      }
+    }
+  }
 }
