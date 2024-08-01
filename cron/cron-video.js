5de63b291617049e6c10043c3aa99212b9f86eb8
kenu <kenu@okjsp.pe.kr>
2024-08-01 22:13:40 +0900

    Revert "whole day"
    This reverts commit 7f2e2a557514da9dd6929a0a420864b5554b5593.

--- cron/cron-video.js
+++ cron/cron-video.js
@@ -2,6 +2,6 @@ import dao from '../youtubeDao.js'
 import vapi from '../services/video.js'
 
 ;(async () => {
-  const list = await dao.findAllChannelList(2914)
+  const list = await dao.findAllChannelList(914)
   list.map((item) => item.channelId).forEach(vapi.addVideos)
 })()
