e11dac098856a8e80d0ca6a78f956a6a5c3b4258
kenu <kenu@okjsp.pe.kr>
2024-08-17 21:26:03 +0900

    🐛 transcript bug fix

--- tests/transcript.test.js
+++ tests/transcript.test.js
@@ -1,7 +1,21 @@
-import fetchTranscript from '../web/utils/transcript.js'
+import { YoutubeTranscript } from "youtube-transcript";
 
-test('', async() => {
-  const videoId = 'ulqHnefBFMM'
-  const transcript = await fetchTranscript(videoId)
-  expect(transcript).not.toBeNull()
-})
+const videoId = "vSIb2sPdu9U";
+test("getTextOnly", async () => {
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+  expect(transcript).not.toBeNull();
+  const fullText = getTextOnly(transcript);
+  expect(fullText).toContain("인데");
+});
+
+function getTextOnly(transcript) {
+  return transcript.map((item) => item.text).join(" ");
+}
+
+test('remove [Object, Object]', async () => {
+  const pattern = /(니다|하죠|하겠죠|고요|까요|네요|데요|세요|아요|어요)\s/g
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId)
+  let fullText = transcript.map((item) => item.text).join(' ')
+  fullText = fullText.replaceAll(pattern, '$1. ')
+  expect(fullText).not.toContain('Object')
+});
--- web/utils/summary.js
+++ web/utils/summary.js
@@ -4,10 +4,10 @@ async function summarize(messages) {
   const openai = new OpenAI()
   const completion = await openai.chat.completions.create({
     messages: messages,
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o-mini',
   })
 
-  return completion.choices
+  return completion.choices[0].message.content
 }
 
 export default summarize
